e482c321fce84690b8621034b3e8016162051337
Pallavi Kokkiligadda <pallavikokkiligadda9@gmail.com>
2026-07-31 20:37:17 +0530

    Module

---
+++ module.py
@@ -0,0 +1,8 @@
+
+import math
+print("The value of cosine is",math.cos(30))
+print("The value of sine is",math.sin(45))
+print("The value of tangent is",math.tan(90))
+print("The SquareRoot Value is",math.sqrt(25))
+print("Tge Power of a given value is",math.pow(2,4))
+print("The value of pi is",math.pi)
